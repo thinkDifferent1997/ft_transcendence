54db51f96b0a7259966d3384c68ca64189f7a6c2
elsikira <elsikira@student.42.fr>
2026-06-26 17:55:50 +0200

    feature: the OAuth 42 is fully implemented, PR incoming

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,9 +4,9 @@
  * Porte d'entrée HTTP de l'authentification.
  * Définit les routes (POST /auth/register, POST /auth/login, ...),
  * reçoit les requêtes, délègue à AuthService et renvoie la réponse.
- * Ne contient aucune logique métier : uniquement l'aiguillage.
  */
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req, Res } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 
@@ -18,4 +18,25 @@ export class AuthController {
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
+
+  /********************** OAUTH 42 API *******************************/
+
+  @Get('42')
+  @UseGuards(AuthGuard('42'))
+  async fortTwoAuth() {
+	
+	  //empty because NestJS never goes inside, the Guard is taking control at this stage to redirect to the 42 Intranet
+  }
+
+  @Get('42/callback')
+  @UseGuards(AuthGuard('42'))
+  async fortyTwoAuthCallback(@Req() req, @Res() res) {
+	  
+	  const userWithToken = await this.authService.loginOrCreate42User(req.user);
+
+	  // here attach the JWT in a secure cookie (ex: res.cookie('jwt'))
+
+	  return res.redirect('https://localhost:8443/quiz');
+  }
+
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,12 +6,21 @@
  * Importé par AppModule.
  */
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { HashingService } from './hashing.service';
+import { FortyTwoStrategy } from './fortytwo.strategy';
 
 @Module({
+	imports: [
+		PassportModule.register({ defaultStrategy: '42'}),
+	],
   controllers: [AuthController],
-  providers: [AuthService, HashingService],
+  providers: [ 
+	  AuthService,
+	  HashingService,
+	  FortyTwoStrategy,
+  ],
 })
 export class AuthModule {}
