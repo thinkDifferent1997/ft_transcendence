7a101b0c1f4c643a7ad56e416307deccff4d1ec2
papacolas <195352446+papacolas@users.noreply.github.com>
2026-08-03 16:36:32 +0200

    Remove unused import of useState

--- frontend/src/app/components/Layout.tsx
+++ frontend/src/app/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { Sparkles, MessageCircle, Send, X, Minus, LogOut, Trophy } from "lucide-react";
-import { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 import { socket } from "../../socket/socket";
