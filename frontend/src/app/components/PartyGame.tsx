b993a22eca2f16177054fa76796210a7ae71d445
namalier <natgomali@DESKTOP-QF7RRTG.localdomain>
2026-08-03 18:34:01 +0200

    ready to pr

--- frontend/src/app/components/PartyGame.tsx
+++ frontend/src/app/components/PartyGame.tsx
@@ -5,8 +5,6 @@ import { useRef } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { useGame } from "../context/GameContext";
 
-import QuestionCard from "./QuestionCard";
-import PlayerBonusPanel from "./PlayerBonus";
 import GamePage from "./GamePage";
 import MatchmakingScreen from "./MatchmakingScreen";
 import ResultsScreen from "./ResultScreen";
